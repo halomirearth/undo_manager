6e4a75ebbab85b166a5e9896c76d6ff66e85d85e
kochmac <maximilian.koch@novartis.com>
2022-01-30 16:11:09 +0100

    KOCHMAX-UNDO-MNGR: Basic test to be able to create UndoManager from createUndoManager

--- src/test/java/undo/ExerciseUndoManagerFactoryTest.java
+++ src/test/java/undo/ExerciseUndoManagerFactoryTest.java
@@ -1,4 +1,19 @@
 package undo;
 
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+
+import org.junit.Test;
+
 public class ExerciseUndoManagerFactoryTest {
+  private static final int DEFAULT_BUFFER_SIZE = 5;
+
+  @Test
+  public void testSuccessfulFactory() {
+    UndoManagerFactory undoManagerFactory = new ExerciseUndoManagerFactory();
+    UndoManager undoManager = undoManagerFactory.createUndoManager(mock(Document.class),
+        DEFAULT_BUFFER_SIZE);
+    assertNotNull(undoManager);
+  }
 }

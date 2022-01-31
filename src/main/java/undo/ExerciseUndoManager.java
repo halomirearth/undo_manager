d1ed1390329b82d6f74daae057f50ef3c11e151b
kochmac <maximilian.koch@novartis.com>
2022-01-31 16:32:37 +0100

    KOCHMAX-UNDO-MNGR: Adding availableUndos & availableRedos Stacks and initialise them in constructor.

--- src/main/java/undo/ExerciseUndoManager.java
+++ src/main/java/undo/ExerciseUndoManager.java
@@ -1,11 +1,18 @@
 package undo;
 
+
+import undo.utils.stack.DropoutStack;
+import undo.utils.stack.Stack;
+
 public class ExerciseUndoManager implements UndoManager {
+  private final Stack<Change> availableUndos;
+  private final Stack<Change> availableRedos;
   private final Document document;
 
   public ExerciseUndoManager(Document document, int bufferSize) {
     this.document = document;
-    this.bufferSize = bufferSize;
+    this.availableUndos = new DropoutStack<>(bufferSize);
+    this.availableRedos = new DropoutStack<>(bufferSize);
   }
 
   @Override
